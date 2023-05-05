23deccb328e4c9301ff75b0177afbbddd57e0ec4
Tina Michl <tinamichl@gmx.de>
2023-05-05 11:28:32 +0200

    add toggle functions

--- index.js
+++ index.js
@@ -2,7 +2,14 @@
 console.clear();
 
 const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
-console.log("xxx");
 bookmarkButton.addEventListener("click", () => {
   bookmarkButton.classList.toggle("bookmark--active");
+});
+
+const cardAnswer = document.querySelector('[data-js="card-answer"]');
+const cardAnswerButton = document.querySelector(
+  '[data-js="card-answer-button"]'
+);
+cardAnswerButton.addEventListener("click", () => {
+  cardAnswer.classList.toggle("card__answer");
 });
