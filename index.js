9488108fe7bca76bad4f2b4bd11ee3fd373b5ce4
Tina Michl <tinamichl@gmx.de>
2023-05-05 11:07:54 +0200

    add toggle bookmark

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // phew… not a lot going on here. Please add some code!
+console.clear();
+
+const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
+console.log("xxx");
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toggle("bookmark--active");
+});
